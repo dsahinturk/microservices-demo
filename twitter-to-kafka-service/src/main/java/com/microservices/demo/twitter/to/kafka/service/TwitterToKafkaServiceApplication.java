9e042989211ac1eee8aa00930771a0048760d56b
Doğukan Şahintürk <dogukan.sahinturk@consultant.turkcell.com.tr>
2021-05-12 14:19:50 +0300

    Course 10: Adding common config module

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,18 +1,20 @@
 package com.microservices.demo.twitter.to.kafka.service;
 
-import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.config.TwitterToKafkaServiceConfigData;
 import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
 @SpringBootApplication
 //public class TwitterToKafkaServiceApplication implements ApplicationListener {
 //public class TwitterToKafkaServiceApplication implements ApplicationEvent {
+@ComponentScan(basePackages = "com.microservices.demo")
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
     
     private  static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
